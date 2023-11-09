91a32b41b809ea514d3a940586b64f7cc7b9cd00
ponza555 <ponpon13173@gmail.com>
2023-11-10 01:01:51 +0700

    fished

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from "@/scenes/home";
 import  Navbar  from "@/scenes/navbar";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses"
+import ContactUs from "@/scenes/contactUs"
+import Footer from "@/scenes/footer"
 
 
 
@@ -32,6 +34,8 @@ function App() {
       <Home setSelectedPage={setSelectedPage}/>
       <Benefits setSelectedPage={setSelectedPage}/>
       <OurClasses setSelectedPage={setSelectedPage}/>
+      <ContactUs setSelectedPage={setSelectedPage}/>
+      <Footer/>
     </div>
     
   )
